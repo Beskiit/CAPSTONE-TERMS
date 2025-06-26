d91102566104b34d6af888ef72abd31e8e897b3f
Airone Gamil <aironegamil123@gmail.com>
2025-06-26 11:00:59 +0800

    Initial commit: Teacher's Management Report System (TERMS) - React login interface

---
+++ TERMS/terms/src/App.jsx
@@ -0,0 +1,84 @@
+import './App.css'
+import depedLogo from './assets/deped-logo.png'
+import EyeIcon from './assets/eye.svg'
+import EyeOffIcon from './assets/eye-close.svg'
+import UserIcon from './assets/user.svg'
+import PasswordIcon from './assets/password.svg'
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <LoginForm />
+      </main>
+    </>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={depedLogo} alt="DepEd Logo" />
+      <h4 className='header-title'>Teacher's Management Report System</h4>
+    </header>
+  )
+}
+
+function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <form>
+      <h2 className='login-title'>Login</h2>
+      <div className="input-container">
+        <div className="input-wrapper">
+          <span className="input-icon"><img src={UserIcon} alt="User" /></span>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+      </div>
+      
+      <div className="input-container">
+        <div className="input-wrapper">
+          <span className="input-icon"><img src={PasswordIcon} alt="Password" /></span>
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="button"
+            className="password-toggle"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <img style={{ height: "1em" }} src={EyeOffIcon} alt="Hide password" /> : <img style={{ height: "1em", paddingLeft: "1em" }} src={EyeIcon} alt="Show password" />}
+          </button>
+        </div>
+        <div className="forgot-password">
+          <a href="/forgot-password">Forgot Password?</a>
+        </div>
+      </div>
+      <div className='login-button-container'>
+        <button type="submit" className='login-button'>Login</button>
+      </div>
+    </form>
+  )
+}
+
+export default App
