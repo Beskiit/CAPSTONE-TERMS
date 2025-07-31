eb64bc48c89faa3b1156afef35d3263c3e439524
Airone Gamil <aironegamil123@gmail.com>
2025-07-31 10:55:26 +0800

    Added a file submission

--- TERMS/terms/src/components/Teacher/ClassificationOfGradesReport.jsx
+++ TERMS/terms/src/components/Teacher/ClassificationOfGradesReport.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from '../shared/Header.jsx';
 import Sidebar from "../shared/Sidebar.jsx";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './ClassificationOfGradesReport.css';
 
 function ClassificationOfGradesReport() {
+    const [openPopup, setOpenPopup] = useState(false);
     return(
         <>
         <Header />
@@ -17,11 +20,28 @@ function ClassificationOfGradesReport() {
                 <div className="content">
                     <div className="buttons">
                         <button>Generate Report</button>
-                        <button>Import File</button>
+                        <button onClick={() => setOpenPopup(true)}>Import File</button>
+                        {openPopup && (
+                            <div className="modal-overlay">
+                                <div className="import-popup">
+                                    <div className="popup-header">
+                                        <h2>Import File</h2>
+                                        <button className="close-button" onClick={() => setOpenPopup(false)}>X</button>
+                                    </div>
+                                    <hr />
+                                    <form className="import-form" onSubmit={(e) => e.preventDefault()}>
+                                        <label htmlFor="fileInput" className="file-upload-label">
+                                            Click here to upload a file
+                                        </label>
+                                        <input id="fileInput" type="file" style={{ display: 'none' }} />
+                                        <button type="submit">Upload</button>
+                                    </form>
+                                </div>
+                            </div>
+                        )}
                         <button>Export</button>
                         <button>Submit</button>
                     </div>
-                    
                 </div>
             </div>
         </div>
